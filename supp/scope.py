16613a0ce84af8de4345bdc0c66fbf31c397c4cd
Anton Bobrov <bobrov@vl.ru>
2014-08-24 11:51:58 +0400

    dry

--- supp/scope.py
+++ supp/scope.py
@@ -5,20 +5,27 @@ from .astwalk import Extractor
 from .util import cached_property
 
 
-class Loc(object):
-    def __init__(self, declared_at):
-        self.declared_at = declared_at
-
-    def __lt__(self, other):
-        return self.declared_at < other.declared_at
-
-
 def create_scope(source, filename=None):
     filename = filename or '<string>'
     tree = parse(source, filename)
     scope = Scope()
     Extractor(scope, tree).process()
     return scope
+
+
+def insert_loc(locations, loc):
+    if locations and locations[-1] < loc:
+        locations.append(loc)
+    elseloc
+        insort(locations, loc)
+
+
+class Loc(object):
+    def __init__(self, declared_at):
+        self.declared_at = declared_at
+
+    def __lt__(self, other):
+        return self.declared_at < other.declared_at
 
 
 class Scope(object):
@@ -35,12 +42,7 @@ class Scope(object):
         self.last_flow.add_name(name)
 
     def add_flow(self, flow):
-        flows = self.flows
-        if flows and flows[-1] < flow:
-            flows.append(flow)
-        else:
-            insort(flows, flow)
-
+        insert_loc(self.flows, flow)
         return flow
 
     @property
@@ -52,39 +54,30 @@ class Scope(object):
         return self.flows[idx].names_at(loc)
 
 
-class Flow(object):
+class Flow(Loc):
     def __init__(self, scope, declared_at, prev=None):
         self.scope = scope
         self.declared_at = declared_at
         self.prev = prev
         self._names = []
 
-    def __lt__(self, other):
-        return self.declared_at < other.declared_at
-
     def add_name(self, name):
-        names = self._names
-        if names and names[-1] < name:
-            names.append(name)
-        else:
-            insort(names, name)
+        insert_loc(self._names, name)
 
     @cached_property
     def names(self):
-        if self.prev:
-            names = self.prev.names.copy()
-        else:
-            names = {}
-
+        names = self.get_prev_names()
         names.update((name.name, name) for name in self._names)
         return names
 
-    def names_at(self, loc):
+    def get_prev_names(self):
         if self.prev:
-            names = self.prev.names.copy()
+            return self.prev.names.copy()
         else:
-            names = {}
+            return {}
 
+    def names_at(self, loc):
+        names = self.get_prev_names()
         idx = bisect(self._names, Loc(loc))
         names.update((name.name, name) for name in self._names[:idx])
         return names
