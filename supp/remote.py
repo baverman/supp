1f24f6e8e0e387991cb57769b04495b1d96a707c
Anton Bobrov <bobrov@vl.ru>
2014-09-14 14:18:20 +0400

    client server protocol (supplement copy/paste)

---
+++ supp/remote.py
@@ -0,0 +1,202 @@
+import sys
+import time
+import os.path
+
+from cPickle import dumps
+from threading import Thread, Lock
+
+
+class Environment(object):
+    """Supplement server client"""
+
+    def __init__(self, executable=None, env=None, logfile=None):
+        """Environment constructor
+
+        :param executable: path to python executable. May be path to virtualenv interpreter
+              start script like ``/path/to/venv/bin/python``.
+
+        :param env: environment variables dict, e.g. ``DJANGO_SETTINGS_MODULE`` value.
+
+        :param logfile: explicit log file, can be passed via environment SUPP_LOG_FILE
+        """
+        self.executable = executable or sys.executable
+        self.env = env
+        self.logfile = logfile
+
+        self.prepare_thread = None
+        self.prepare_lock = Lock()
+
+    def _run(self):
+        from subprocess import Popen
+        from multiprocessing.connection import Client, arbitrary_address
+
+        if sys.platform == 'win32':
+            addr = arbitrary_address('AF_PIPE')
+        else:
+            addr = arbitrary_address('AF_UNIX')
+
+        supp_server = os.path.join(os.path.dirname(__file__), 'server.py')
+        args = [self.executable, supp_server, addr]
+
+        env = os.environ.copy()
+        if self.env:
+            env.update(self.env)
+
+        if self.logfile and 'SUPP_LOG_FILE' not in env:
+            env['SUPP_LOG_FILE'] = self.logfile
+
+        self.proc = Popen(args, env=env)
+
+        start = time.time()
+        while True:
+            try:
+                self.conn = Client(addr)
+            except Exception, e:
+                if time.time() - start > 5:
+                    raise Exception('Supp server launching timeout exceed: ' + str(e))
+
+                time.sleep(0.3)
+            else:
+                break
+
+    def _threaded_run(self):
+        try:
+            self._run()
+        finally:
+            self.prepare_thread = None
+
+    def prepare(self):
+        with self.prepare_lock:
+            if self.prepare_thread:
+                return
+
+            if hasattr(self, 'conn'):
+                return
+
+            self.prepare_thread = Thread(target=self._threaded_run)
+            self.prepare_thread.start()
+
+    def run(self):
+        with self.prepare_lock:
+            if self.prepare_thread:
+                self.prepare_thread.join()
+
+            if not hasattr(self, 'conn'):
+                self._run()
+
+    def _call(self, name, *args, **kwargs):
+        try:
+            self.conn
+        except AttributeError:
+            self.run()
+
+        self.conn.send_bytes(dumps((name, args, kwargs), 2))
+        result, is_ok = self.conn.recv()
+
+        if is_ok:
+            return result
+        else:
+            raise Exception(result)
+
+    # def lint(self, project_path, source, filename, syntax_only=False):
+    #     return self._call('lint', project_path, source, filename, syntax_only)
+
+    # def check_syntax(self, source):
+    #     """Checks source syntax against current environment
+
+    #     :param source: unicode or byte string code source
+    #     :returns: None if syntax is valid and tuple ((lineno, offset), error_message) otherwise
+    #     """
+    #     return self._call('check_syntax', source)
+
+    def assist(self, project_path, source, position, filename):
+        """Return completion match and list of completion proposals
+
+        :param project_path: absolute project path
+        :param source: code source
+        :param position: tuple of (line, column)
+        :param filename: absolute path of file with source code
+        :returns: tuple (completion match, sorted list of proposals)
+        """
+        return self._call('assist', project_path, source, position, filename)
+
+    # def get_location(self, project_path, source, position, filename):
+    #     """Return line number and file path where name under cursor is defined
+
+    #     If line is None location wasn't finded. If file path is None, defenition is located in
+    #     the same source.
+
+    #     :param project_path: absolute project path
+    #     :param source: unicode or byte string code source
+    #     :param position: character or byte cursor position
+    #     :param filename: absolute path of file with source code
+    #     :returns: tuple (lineno, file path)
+    #     """
+    #     return self._call('get_location', project_path, source, position, filename)
+
+    # def get_docstring(self, project_path, source, position, filename):
+    #     """Return signature and docstring for current cursor call context
+
+    #     Some examples of call context::
+
+    #        func(|
+    #        func(arg|
+    #        func(arg,|
+
+    #        func(arg, func2(|    # call context is func2
+
+    #     Signature and docstring can be None
+
+    #     :param project_path: absolute project path
+    #     :param source: unicode or byte string code source
+    #     :param position: character or byte cursor position
+    #     :param filename: absolute path of file with source code
+    #     :returns: tuple (signarure, docstring)
+    #     """
+    #     return self._call('get_docstring', project_path, source, position, filename)
+
+    def configure_project(self, project_path, config):
+        """Reconfigure project
+
+        :param project_path: absolute project path
+        :param config: dict with config key/values
+        """
+        return self._call('configure_project', project_path, config)
+
+    # def get_scope(self, project_path, source, lineno, filename, continous=True):
+    #     """
+    #     Return scope name at cursor position
+
+    #     For example::
+
+    #         class Foo:
+    #             def foo(self):
+    #                 pass
+    #                 |
+    #             def bar(self):
+    #                 pass
+
+    #     get_scope return Foo.foo if continuous is True and Foo otherwise.
+
+    #     :param project_path: absolute project path
+    #     :param source: unicode or byte string code source
+    #     :param position: character or byte cursor position
+    #     :param filename: absolute path of file with source code
+    #     :param continous: allow parent scope beetween children if False
+    #     """
+    #     return self._call('get_scope', project_path, source, lineno, filename, continous=continous)
+
+    def eval(self, source):
+        return self._call('eval', source)
+
+    def close(self):
+        """Shutdown server"""
+
+        try:
+            self.conn
+        except AttributeError:
+            pass
+        else:
+            self.conn.send_bytes(dumps(('close', (), {}), 2))
+            self.conn.close()
+            del self.conn
---
+++ supp/server.py
@@ -0,0 +1,122 @@
+import sys
+import os.path
+import logging
+
+logger = logging.getLogger('server')
+
+try:
+    from cPickle import loads, dumps
+except ImportError:
+    from pickle import loads, dumps
+
+try:
+    import supp
+except ImportError:
+    fname = os.__file__
+    if fname.endswith('.pyc'):
+        fname = fname[:-1]
+
+    if not os.path.islink(fname):
+        raise
+
+    real_prefix = os.path.dirname(os.path.realpath(fname))
+    site_packages = os.path.join(real_prefix, 'site-packages')
+    old_path = sys.path
+    sys.path = old_path + [site_packages]
+    try:
+        import supp
+    finally:
+        sys.path = old_path
+
+from supp.project import Project
+from supp.assistant import assist
+
+
+class Server(object):
+    def __init__(self, conn):
+        self.conn = conn
+        self.projects = {}
+        self.configs = {}
+        # self.monitor = get_monitor()
+        # self.monitor.start()
+
+    def configure_project(self, path, config):
+        self.configs[path] = config
+        self.projects[path] = self.create_project(path)
+
+    def create_project(self, path):
+        config = self.configs.get(path, {})
+        config.setdefault('hooks', []).insert(0, 'supplement.hooks.override')
+        p = Project(path, self.configs.get(path, {}))
+        return p
+
+    def get_project(self, path):
+        try:
+            return self.projects[path]
+        except KeyError:
+            pass
+
+        p = self.projects[path] = self.create_project(path)
+        return p
+
+    def process(self, name, args, kwargs):
+        try:
+            is_ok = True
+            result = getattr(self, name)(*args, **kwargs)
+        except Exception as e:
+            logger.exception('%s error', name)
+            is_ok = False
+            result = e.__class__.__name__, str(e)
+
+        return result, is_ok
+
+    def assist(self, path, source, position, filename):
+        return assist(self.get_project(path), source, position, filename)
+
+    def eval(self, source):
+        ctx = {}
+        source = '\n'.join('    ' + r for r in source.splitlines())
+        source = 'def boo():\n{}\nresult = boo()'.format(source)
+        exec source in ctx
+        return ctx['result']
+
+    def run(self):
+        conn = self.conn
+        while True:
+            if conn.poll(1):
+                try:
+                    args = loads(conn.recv_bytes())
+                except EOFError:
+                    break
+                except Exception:
+                    logger.exception('IO error')
+                    break
+
+                if args[0] == 'close':
+                    conn.close()
+                    break
+                else:
+                    result, is_ok = self.process(*args)
+                    try:
+                        self.conn.send_bytes(dumps((result, is_ok), 2))
+                    except:
+                        logger.exception('Send error')
+
+if __name__ == '__main__':
+    from multiprocessing.connection import Listener
+
+    if 'SUPP_LOG_LEVEL' in os.environ:
+        level = int(os.environ['SUPP_LOG_LEVEL'])
+    else:
+        level = logging.ERROR
+
+    if 'SUPP_LOG_FILE' in os.environ:
+        logging.basicConfig(filename=os.environ['SUPP_LOG_FILE'],
+            format="%(asctime)s %(name)s %(levelname)s: %(message)s", level=level)
+    else:
+        logging.basicConfig(format="%(name)s %(levelname)s: %(message)s", level=level)
+
+    listener = Listener(sys.argv[1])
+    conn = listener.accept()
+    server = Server(conn)
+    server.run()
